2cfcbce2bbe8d59c5b31c311d5283d4ab0c875f5
Marco Matildo <marcomatildo@gmail.com>
2019-11-18 19:15:22 +0800

    :sparkles: add new component

--- src/components/Home/RandomMeal.vue
+++ src/components/Home/RandomMeal.vue
@@ -1,11 +1,13 @@
 <template>
   <b-container>
     <div class="random-meal pb-3">
-      <h3 class="bordered-bottom pb-4">More to <span class="text-light-green">Explore</span></h3>
+      <h3 class="bordered-bottom pb-4">Random <span class="text-light-green">Meals</span></h3>
     </div>
     <b-row align-h="center">
-      <b-col class="p-4 col-meals" sm="4" v-for="meal in meals" :key="meal.idMeal">
-        <img fluid :src="meal.strMealThumb" alt="Random Meal Image">
+      <b-col class="p-4 col-meals" sm="3" v-for="meal in meals" :key="meal.idMeal">
+        <router-link :to="`/meals/${meal.strMeal}`">
+          <img fluid :src="meal.strMealThumb" alt="Random Meal Image">
+        </router-link>
         <p class="pt-2">{{ meal.strMeal }}</p>
       </b-col>
     </b-row>
@@ -16,7 +18,7 @@
 export default {
   data () {
     return {
-      api: 'https://www.themealdb.com/api/json/v1/1/random.php',
+      randomMealApi: 'https://www.themealdb.com/api/json/v1/1/random.php',
       meals: []
     }
   },
@@ -27,16 +29,16 @@ export default {
     async getRandonMeals () {
       try {
         let mealCount = this.meals.length
-        while (mealCount !== 9) {
-          let res = await fetch(this.api)
+        while (mealCount !== 12) {
+          let res = await fetch(this.randomMealApi)
           let data = await res.json()
+          // Check for duplicates
           let isExist = !!this.meals.find(meal => JSON.stringify(meal) === JSON.stringify(data.meals[0]))
           if (!isExist) {
             this.meals.push(data.meals[0])
             mealCount = this.meals.length
           }
         }
-        this.count = this.meals.length
       } catch (err) {
         console.log(`Error from random meals API: ${err}`)
       }
