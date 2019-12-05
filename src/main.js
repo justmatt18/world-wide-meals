9a5bb7f1fdf197a49a918a9cd99b70ae188c28b7
Marco Matildo <marcomatildo@gmail.com>
2019-12-05 22:14:23 +0800

    :sparkles: enable vue youtube embed

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueYouTubeEmbed from 'vue-youtube-embed'
 import router from './router'
 import store from './store'
 
 Vue.use(BootstrapVue)
+Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
 new Vue({
